cbbd13e9ee2c1fae685ebf21c4f17766f1efd025
HercaiErdem <111076131+HercaiErdem@users.noreply.github.com>
2024-02-29 16:58:50 +0300

    29.02.2023 v1

--- cypress/e2e/udemy-cypress-egitim/cypressAliasThenEachWrap.cy.js
+++ cypress/e2e/udemy-cypress-egitim/cypressAliasThenEachWrap.cy.js
@@ -6,14 +6,24 @@ describe('Alias, Then, Each and Wrap', () => {
 
         cy.visit("https://shopist.io/")
 
-        cy.get(".navbar-section a").as("Navbar-Menu") // Alias sample
-        cy.get("@Navbar-Menu").then(($el) => {
+        cy.get(".navbar-section a").should("be.visible").as("Navbar-Menu") // Alias sample
+        cy.get("@Navbar-Menu")
+        .then(($el) => {
             itemlenght = $el.leght
-        }).each(($el, index) =>{
+        })
+        .each(($el, index) =>{
             // cy.log($el.text())
+            cy.get("@Navbar-Menu").should("be.visible")
             cy.log('Navgar-Menu ' + index + ": " + $el.text());
+        
+
+            if ($el.text().includes("Cart")) { // wrap ornegi
+                cy.wrap($el).click()
+            
+            }
         })
 
+        
 
     });
 
