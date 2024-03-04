99ab7a760cdf03e92e1d43c3c501f655545b44ff
HercaiErdem <111076131+HercaiErdem@users.noreply.github.com>
2024-03-04 16:33:33 +0300

    04.03.2024

--- cypress/e2e/udemy-cypress-egitim/cypressFakerjs.cy.js
+++ cypress/e2e/udemy-cypress-egitim/cypressFakerjs.cy.js
@@ -4,3 +4,15 @@ const { faker } = require("@faker-js/faker")
 let fakeEmail = faker.internet.email()
 let fakePassword = faker.internet.password()
 let fakeName = faker.person.firstName()
+
+describe('Using of FakerJS', () => {
+
+    it('Testing sample with fakerjs', () => {
+        cy.visit("https://automationexercise.com/login")
+
+        cy.get("input[data-qa='login-email']").type(fakeEmail)
+        cy.get("input[data-qa='login-password']").type(fakePassword)
+        cy.get("button[data-qa='login-button']").click()
+    });
+    
+});
